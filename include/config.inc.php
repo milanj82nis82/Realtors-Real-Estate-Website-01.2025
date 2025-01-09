63dafa83f9c934753496e2c1b733815962c89931
Milan Janković <milanj82nis82@gmail.com>
2025-01-09 19:58:40 +0100

    a

--- include/config.inc.php
+++ include/config.inc.php
@@ -22,8 +22,8 @@ define ( 'SITE_NAME'  , 'Real Estate' ); // Site Name
 ____________________________     */
 define ( 'PHPMAILER_HOST'  , 'sandbox.smtp.mailtrap.io' ); // Hostname
 define ( 'PHPMAILER_PORT'  , 2525 ); //  Database Names
-define ( 'PHPMAILER_USERNAME'  , '' ); // Username
-define ( 'PHPMAILER_PASSWORD'  , '' ); // Password
+define ( 'PHPMAILER_USERNAME'  , '7ec9948ebe4ec6' ); // Username
+define ( 'PHPMAILER_PASSWORD'  , 'd24f60ae3f8bd8' ); // Password
 
 
 /*      ADMIN CONFIGURATION 
